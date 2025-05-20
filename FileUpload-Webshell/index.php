3c5fc9b9808615d85117bd966621e7cb64c5f390
nahong_c <evangelion14125@gmail.com>
2025-05-20 10:54:23 +0900

    feat : add FIleUpload-Webshell challenge
    FIleUpload-Webshell

---
+++ FileUpload-Webshell/index.php
@@ -0,0 +1,5 @@
+<h2>Upload your file</h2>
+<form method="POST" enctype="multipart/form-data" action="upload.php">
+  <input type="file" name="file"><br><br>
+  <input type="submit" value="Upload">
+</form>
